198e31169de942e419490fd6be45c320b6bb6bc8
Drew Wells <drew.wells00@gmail.com>
2011-09-30 12:10:52 -0400

    New styling and effects

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-/* Author: 
+/* Author: Drew Wells
 
 */
+var $ = window.$;
+
+$( "#container" ).removeClass( 'see-through' );
+
+$( 'a' ).one( 'click', function( event ){
+
+
+    var anchor = this;
+    $( '#container' ).addClass( 'see-through' );
+    setTimeout(function(){
+        $( '#container' ).removeClass( 'see-through' );
+        anchor.click();
+    }, 2000 );
+
+    return false;
+});
 
 
 
